4109fe5229f05c10f360a318c1d08711ec684d36
UnicsYu <unics@ltspgzs.cn>
2023-01-27 21:40:27 -0800

    [feat] test for so

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <iostream>
 using namespace cv;
-int main(int argc, char **argv)
+extern double compareImg(std::string answer, std::string key)
 {
-    if (argc != 3)
-    {
-        std::cout << -1 << std::flush;
-        return 1;
-    }
-
     std::string image_path1;
     std::string image_path2;
 
@@ -18,21 +11,19 @@ int main(int argc, char **argv)
 
     try
     {
-        image_path1 = samples::findFile(argv[1]);
-        image_path2 = samples::findFile(argv[2]);
+        image_path1 = samples::findFile(answer);
+        image_path2 = samples::findFile(key);
         img1 = imread(image_path1, IMREAD_COLOR);
         img2 = imread(image_path2, IMREAD_COLOR);
     }
     catch (Exception &e)
     {
-        std::cerr << -2 << std::flush;
-        return 2;
+        return -3;
     }
 
     if (img1.empty() || img2.empty())
     {
-        std::cerr << -3 << std::flush;
-        return 2;
+        return -2;
     }
     Mat out;
     absdiff(img1, img2, out);
@@ -41,6 +32,5 @@ int main(int argc, char **argv)
     diff += sum(out)[1];
     diff += sum(out)[2];
     diff += sum(out)[3];
-    std::cout << diff << std::flush;
-    return 0;
+    return diff;
 }
